687b4284cfcbe6f5da25340bb7628f9dc69c4843
Anthony <a.th.nguyen30@gmail.com>
2018-03-18 15:30:55 -0700

    sendgrid api added

---
+++ common/sendgrid.py
@@ -0,0 +1,42 @@
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+sg = sendgrid.SendGridAPIClient(apikey='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')
+
+def send_mail (from_email_str, to_email_str, subject=None, message=None, html=None, cc=None):
+	
+	if html:
+		content = [{
+			"type": "text/html",
+			"value": html
+		}]
+	else:
+		content = [
+			{
+				"type":"text/plain"
+				"value": message
+			}
+		]
+
+	cc_emails = [{'email': email} for email in cc] if cc else []
+
+	data = {
+	  	"personalizations": [
+		    {
+		      	"to": [{
+		          	"email": from_email_str
+		        }],
+		      	"subject": subject
+		    }
+	  	],
+	  	"from": {
+	    	"email": to_email_str
+	  	},
+	  	"content": content,
+	  	"cc": cc_emails
+	}
+
+	response = sg.client.mail.send.post(request_body=data)
+	return response
+	
--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.core.exceptions import SuspiciousOperation
 from django.http import HttpResponse
-
+from common.sendgrid import send_mail
 import stripe
 
 stripe.api_key = "sk_live_G0h2V0jwMQhp3SYNlpyX61A3"
@@ -9,9 +9,17 @@ stripe.api_key = "sk_live_G0h2V0jwMQhp3SYNlpyX61A3"
 def create_charge(req):
 	try:
 		stripe.Charge.create(amount=req.amount, source=req.source, description=req.description, currency='usd')
+		send_mail(
+			from_email_str='admin@treatsti.com', 
+			to_email_str='drphan@firstdoc.co', 
+			cc=['anthony@firstdoc.co', 'tracy@firstdoc.co']
+			message=description,
+			subject='Payment charge created'
+		)
 		return HttpResponse(status=200)
 	except: 
 		return HttpResponse(status=400)
+
 
 handlers = {
 	'POST': create_charge
